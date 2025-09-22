9ba5627df5fd4ad9521470f3de4488b4790956b8
Philip Lambok <philiplambok@gmail.com>
2025-09-22 20:39:20 +0700

    fix test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Use inline job processing for tests (no Redis dependency)
+  config.active_job.queue_adapter = :inline
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
