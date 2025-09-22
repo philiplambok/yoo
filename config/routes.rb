1d9f892ea942ae815700ea3f2ad755e99abd287a
Philip Lambok <philiplambok@gmail.com>
2025-09-22 20:44:08 +0700

    fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :messages
 
   # Sidekiq Web UI
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+  require "sidekiq/web"
+  mount Sidekiq::Web => "/sidekiq"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- test/jobs/create_random_users_job_test.rb
+++ test/jobs/create_random_users_job_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class CreateRandomUsersJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create 5 random users" do
+    initial_user_count = User.count
+
+    # In test environment, jobs run inline (synchronously)
+    CreateRandomUsersJob.perform_now
+
+    assert_equal initial_user_count + 5, User.count
+  end
+
+  test "should perform job immediately with inline adapter" do
+    initial_user_count = User.count
+
+    # With inline adapter, perform_later executes immediately
+    CreateRandomUsersJob.perform_later
+
+    # Job should have completed immediately, creating 5 users
+    assert_equal initial_user_count + 5, User.count
+  end
 end
